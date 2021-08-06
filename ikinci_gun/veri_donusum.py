26f632673f6e993409b13baf487d8195813b8cca
cagriyalniz <cagriyalniz@DESKTOP-I5C70L2>
2021-08-06 14:22:16 +0300

    06.08 calismalari

---
+++ ikinci_gun/veri_donusum.py
@@ -0,0 +1,13 @@
+# veri tipi donusumleri
+
+a = input('ilk sayi ')
+b = input('ikinci sayi ')
+
+print(type(a))
+print(type(b))
+
+toplam = a + b
+print(toplam)
+
+toplam2 = int(a) + int(b)
+print(toplam2)
